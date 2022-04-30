2925e99b1c21de78275d8657e1a4d44b958ab6cb
zhaohui song <zhaohui.song@studio.unibo.it>
2022-04-30 16:02:36 +0200

    add load existing model

--- envtest/python/run_vision_ppoTestCompass.py
+++ envtest/python/run_vision_ppoTestCompass.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.WARNING)
 ##########--COSTANT VALUES--##########
 ######################################
 
-ENVIRONMENT_CHANGE_THRESHOLD = 5000000000
+ENVIRONMENT_CHANGE_THRESHOLD = 50000
 
 
 cfg = YAML().load(
@@ -48,6 +48,7 @@ def parser():
     parser.add_argument("--render", type=int, default=0, help="Render with Unity")
     parser.add_argument("--trial", type=int, default=1, help="PPO trial number")
     parser.add_argument("--iter", type=int, default=100, help="PPO iter number")
+    parser.add_argument("--load", type=str, default="", help="load and train an existing model.")
     return parser
 
 
@@ -98,49 +99,52 @@ def main():
     #################################################
     ###############--SETUP PPO-MODEL--###############
     #################################################
+    if args.load:
+        load_path = semipath + "/PPO_" + args.load + "/best_model/best_model.zip"
+        model = PPO.load(load_path, env=train_env, device='cuda:0', custom_objects=None, print_system_info=True, force_reset=True)
+    else:
+        model = PPO(
+            tensorboard_log=log_dir,
+            policy="MultiInputPolicy",
+            policy_kwargs=dict(
+                features_extractor_class=CompassModel,
+                features_extractor_kwargs=dict(linear_prob=True,
+                                               pretrained_encoder_path=os.environ["COMPASS_CKPT"],
+                                               feature_size=269),
+                # features_extractor_class=SimpleCNNFE,
+                # features_extractor_kwargs=dict(
+                #        features_dim=256),
+                activation_fn=torch.nn.ReLU,
+                net_arch=[269, dict(pi=[269, 134, 67], vf=[269, 134, 67])],
+                # Number hidden layer 1-3 TODO last layer?
+                log_std_init=-0.5,
+                normalize_images=False,
+                # optimizer_kwargs=dict(weight_decay=0, betas=0.9),  # Adam optimizer TODO
+            ),
 
-    model = PPO(
-        tensorboard_log=log_dir,
-        policy="MultiInputPolicy",
-        policy_kwargs=dict(
-            features_extractor_class=CompassModel,
-            features_extractor_kwargs=dict(linear_prob=True,
-                                           pretrained_encoder_path=os.environ["COMPASS_CKPT"],
-                                           feature_size=269),
-            # features_extractor_class=SimpleCNNFE,
-            # features_extractor_kwargs=dict(
-            #        features_dim=256),
-            activation_fn=torch.nn.ReLU,
-            net_arch=[269, dict(pi=[269, 134, 67], vf=[269, 134, 67])],
-            # Number hidden layer 1-3 TODO last layer?
-            log_std_init=-0.5,
-            normalize_images=False,
-            # optimizer_kwargs=dict(weight_decay=0, betas=0.9),  # Adam optimizer TODO
-        ),
+            env=train_env,
+            gamma=0.98,  # Discout factor old 0.99 IMPORTANT 0.8,0.9997-0.99
+            seed=args.seed,
+            ent_coef=0.002,  # Range:  0 - 0.01
+            vf_coef=0.75,  # OLD 0.5 Range 0.5-1
+            max_grad_norm=0.5,
+            clip_range=0.25,  # OLD 0.2
+            learning_rate=0.001,  # OLD 0.0003 Range: 1e-5 - 1e-3
+            gae_lambda=0.9,  # OLD 95 Range 0.9-1
+            use_sde=False,  # action noise exploration vs GsDSE(true)
+            target_kl=None,  # Range: 0.003 - 0.03 IMPORTANT?? TODO
+            verbose=1,
+            n_epochs=10,  # Range: 3 - 30
+            batch_size=40,  # num batch != num env!! to use train env, as eval env need to use 1 num env!
+            n_steps=20,  # Ragne: 512-5000
 
-        env=train_env,
-        gamma=0.98,  # Discout factor old 0.99 IMPORTANT 0.8,0.9997-0.99
-        seed=args.seed,
-        ent_coef=0.002,  # Range:  0 - 0.01
-        vf_coef=0.75,  # OLD 0.5 Range 0.5-1
-        max_grad_norm=0.5,
-        clip_range=0.25,  # OLD 0.2
-        learning_rate=0.001,  # OLD 0.0003 Range: 1e-5 - 1e-3
-        gae_lambda=0.9,  # OLD 95 Range 0.9-1
-        use_sde=False,  # action noise exploration vs GsDSE(true)
-        target_kl=None,  # Range: 0.003 - 0.03 IMPORTANT?? TODO
-        verbose=1,
-        n_epochs=10,  # Range: 3 - 30
-        batch_size=300,  # num batch != num env!! to use train env, as eval env need to use 1 num env!
-        n_steps=300,  # Ragne: 512-5000
+            # env_cfg=cfg, OLD PPO
+            # eval_env=train_env, OLD PPO
+            # eval_env=eval_env, OLD PPO
+            # use_tanh_act=True, OLD PPO
+            # gae_lambda=0.95,
 
-        # env_cfg=cfg, OLD PPO
-        # eval_env=train_env, OLD PPO
-        # eval_env=eval_env, OLD PPO
-        # use_tanh_act=True, OLD PPO
-        # gae_lambda=0.95,
-
-    )
+        )
 
     model.learn(total_timesteps=int(5 * 1e7), log_interval=5,
                 callback=[custom_callback, eval_callback, checkpoint_callback])
